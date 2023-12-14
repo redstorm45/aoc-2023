f45795b31469fdbcbdaaa055176c8f152eff733e
Pierre Cadart <pierre.cadart@gmail.com>
2023-12-14 23:02:48 +0100

    Day 14

--- aoc14/src/main.rs
+++ aoc14/src/main.rs
@@ -1,6 +1,7 @@
 
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 
 fn transpose(v: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     assert!(!v.is_empty());
@@ -39,7 +40,7 @@ fn roll_line_left(blocks: &Vec<bool>, rolling: &Vec<bool>) -> Vec<bool> {
 }
 
 fn roll_platform_west(blocks: &Vec<Vec<bool>>, rolling: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
-    &blocks.iter()
+    blocks.iter()
         .zip(rolling.iter())
         .map(|(b,r)| roll_line_left(b,r))
         .collect()
@@ -47,33 +48,56 @@ fn roll_platform_west(blocks: &Vec<Vec<bool>>, rolling: &Vec<Vec<bool>>) -> Vec<
 
 fn roll_platform_up(blocks: &Vec<Vec<bool>>, rolling: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     transpose(
-        roll_platform_west(
-            transpose(blocks),
-            transpose(rolling)
+        &roll_platform_west(
+            &transpose(&blocks),
+            &transpose(&rolling)
         )
     )
 }
 
 fn rotate_platform_cw(m: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     // [N,W,S,E] -> [E,S,W,N]
-    transpose(m.iter().rev().collect())
+    let rev: Vec<Vec<bool>> = m.iter().rev().cloned().collect();
+    transpose(&rev)
 }
 
 fn rotate_platform_ccw(m: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     // [N,W,S,E] -> [W,S,E,N]
     // [N,W,S,E] ---transpose--> [W,N,E,S] ---mirror U/D--> [W,S,E,N]
-    transpose(m).iter().rev().collect()
+    transpose(m).into_iter().rev().collect()
 }
 
 fn roll_platform_round(blocks: &Vec<Vec<bool>>, rolling: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     // roll towards N,W,S,E
-    let mut r = rotate_platform_ccw(rolling);
+    /*
+    println!("source");
+    printblock(&blocks,&rolling);
+    */
+
+    let mut r = rotate_platform_ccw(&rolling);
+    let mut b = rotate_platform_ccw(&blocks);
+
+    /*
+    println!("CCW");
+    printblock(&b,&r);
+    */
 
     for _ in 0..4 {
-        r = rotate_platform_cw( roll_platform_west(r) );
+        r = rotate_platform_cw( &roll_platform_west(&b, &r) );
+        b = rotate_platform_cw(&b);
+        /*
+        println!("CW");
+        printblock(&b,&r);
+        */
     }
 
-    return rotate_platform_ccw(r);
+    let res = rotate_platform_cw(&r);
+    /*
+    println!("CW");
+    printblock(&b,&r);
+    println!("-------");
+    */
+    return res;
 }
 
 fn platform_weight(rolling: &Vec<Vec<bool>>) -> usize {
@@ -86,6 +110,21 @@ fn platform_weight(rolling: &Vec<Vec<bool>>) -> usize {
         ).sum()
 }
 
+fn printblock(block: &Vec<Vec<bool>>, roll: &Vec<Vec<bool>>) {
+    for (lineb, liner) in block.iter().zip(roll.iter()) {
+        for (b,r) in lineb.iter().zip(liner.iter()) {
+            if *b {
+                print!("#");
+            } else if *r {
+                print!("O");
+            } else {
+                print!(" ");
+            }
+        }
+        println!("");
+    }
+}
+
 fn main() {
     let mut args = env::args();
     args.next();
@@ -93,13 +132,34 @@ fn main() {
 
     let contents = fs::read_to_string(filename).expect("Could not read file");
 
+    //let contents = "O....#....\nO.OO#....#\n.....##...\nOO.#O....O\n.O.....O#.\nO.#..O.#.#\n..O..#O..O\n.......O..\n#....###..\n#OO..#....";
+
     let lines = contents.split('\n').filter(|s| s.len()>0).collect();
     let blocks = find_shapes(&lines, &'#');
     let rolling = find_shapes(&lines, &'O');
 
-    let rolled = roll_platform_up(&blocks, &rolling);
+    let rolledup = roll_platform_up(&blocks, &rolling);
 
-    let w = platform_weight(&rolled);
-
+    let w = platform_weight(&rolledup);
     println!("Weight: {w}");
+
+    let mut known: HashMap<Vec<Vec<bool>>,i32> = HashMap::new();
+    let mut current: Vec<Vec<bool>> = rolling;
+    let mut step = 0;
+    while !known.contains_key(&current) {
+        known.insert(current.clone(), step);
+        current = roll_platform_round(&blocks, &current);
+        step += 1;
+    }
+    let mut loopstep = known.get(&current).unwrap();
+    // loop happens from 'loop' to 'loopstep'
+    // go to cycle before the end
+    let remaining_steps = (1000000000-loopstep)%(step-loopstep);
+    for i in 0..remaining_steps {
+        current = roll_platform_round(&blocks, &current);
+    }
+
+    let w2 = platform_weight(&current);
+    println!("Weight 2: {w2}");
+    // 1345 too low
 }
